8a2bee0aa1e7b6dfec2987fa75806c07257a6338
Asbin Katwal <katwalasbin79@gmail.com>
2025-05-15 16:35:49 +0545

    logout update

--- canteen_management_system/settings.py
+++ canteen_management_system/settings.py
@@ -171,14 +171,14 @@ AUTH_USER_MODEL = 'canteen.User'
 
 
 
+from decouple import config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 
 
